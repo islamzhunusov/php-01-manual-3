c9af0cac3006c1ebab50194adf64d96fc3d4ff0d
islamzhunusov <tywkan89@gmail.com>
2016-10-20 15:00:06 +0600

    Test1

---
+++ index.php
@@ -0,0 +1,54 @@
+
+<?php
+//$title="Main list";
+//require "header.php";
+//echo "Telo Document";
+//require "footer.php";
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="content-type" charset="utf-8">
+    <title>Интернет магазин</title>
+    <link href="style/style.css" rel="stylesheet" type="text/css">
+    <link href="style/bootstrap.css" rel="stylesheet" type="text/css" >
+    <link href="cart-ico.png" rel="shortcut icon" type="image/x-icon"/>
+    <script src="js/bootstrap.js"></script> 
+    <script type="text/javascript">$(function(){$('.allProjects').click(function(){$('.overlay').fadeIn('fast',function(){$('.nonebox').animate({'top':'30%'},500);});});$('.box-close').click(function(){$('.nonebox').animate({'top':'-1000px'},500,function(){$('.overlay').fadeOut('fast');});});});</script>
+    <script src="style/jquery-3.1.1.min.js"></script>
+</head>
+<body>
+    <header>
+        <div id="logoHeader">
+<a href="#" title="Интернет магазин"><img src="cart-ico.png" alt="Интернет магазин" title="Интернет магазин"/>Магазин</a>
+</div>
+        <table id="head_table">
+<tr>
+<td id="secondMenuHead" colspan="2">
+        <object>
+<nav class="ph-lift1">
+<ul>
+<li class="active"><a href="#" data-title="На главную"><span data-title="На главную">Главная</span></a>
+</li>
+<li><a href="#"><span data-title="Короткие посты">Лента новостей</span></a>
+</li>
+<li><a href="#"><span data-title="Интересные вещи"><center>Магазин</center></span></a>
+<ul>
+<li><a href="#"><span data-title="Разделы магазина">Разделы</span></a></li>
+<li><a href="#"><span data-title="Информация о магазине">О магазине</span></a></li>
+<li><a href="#"><span data-title="Гарантии магазина">Гарантии</span></a></li>
+<li><a href="#"><span data-title="Информация о доставке">Доставка</span></a></li>
+<li><a href="#"><span data-title="Вопросы и ответы">Помощь</span></a></li>
+</ul>
+    </li>
+        </ul>
+            </nav>
+        </object>
+        </td>
+</tr>
+</table>
+    </header>
+    <article> </article>
+    <footer> </footer>
+</body>    
+</html>
